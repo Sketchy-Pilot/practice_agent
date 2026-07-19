ca752efa64dbcaf133444a4b2fa6df81b6f4b5c9
Curtis Limbach <climbach@gmail.com>
2026-07-19 16:55:18 -0500

    CH2 L3 List Files.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -9,6 +9,13 @@ def get_files_info(working_directory: str, directory: str = ".") -> str:
             return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
         if os.path.isdir(target_dir) is False:
             return f'Error: "{target_dir}" is not a directory'
-        return f'Success: "{directory}" is within the working directory'
+        #return f'Success: "{directory}" is within the working directory'
+        dir_list: list[str] = os.listdir(target_dir)
+        output = f"Result for '{directory}' directory:"
+        output = output.replace("'.'", "current")
+        for i in range(0,len(dir_list)):
+            target = os.path.normpath(os.path.join(target_dir,dir_list[i]))
+            output += f"\n  - {dir_list[i]}: file_size={os.path.getsize(target)} bytes, is_dir={os.path.isdir(target)}"
+        return output
     except Exception as e:
-        return f'error: {e}'
+        return f'Error: {e}'
