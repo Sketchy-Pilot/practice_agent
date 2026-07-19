1ec8d15469c2868cda7474dc9194e6f393d9addc
Curtis Limbach <climbach@gmail.com>
2026-07-19 10:12:06 -0500

    L2 adding some imports, lesson incomplete

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import os
 from dotenv import load_dotenv
+from openai import OpenAI
+from openai.types import Completion, CompletionChoice, CompletionUsage
 
 load_dotenv()
 api_key = os.environ.get("OPENROUTER_API_KEY")
 
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
+
+if api_key is None:
+    raise RuntimeError("API_KEY environment variable not found.")
+
+client.chat.completions.create(model, messages):
+
 
 def main():
     print("Hello from practice-agent!")
+
 
 
 if __name__ == "__main__":
